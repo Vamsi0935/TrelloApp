7a396d218e03406ad8ca0abbcd2e1de2b3f80645
Vamsikrishna <141436747+Vamsi0935@users.noreply.github.com>
2024-09-19 22:08:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000","https://trello-app-frontend-mocha.vercel.app",
+    methods:["GET","POST","PUT","DELETE"],
     credentials: true,
   })
 );
